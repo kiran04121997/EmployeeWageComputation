162297e66f7ed2fea3cfa64995bd49a418d44b1b
kiran04121997 <bhosalekiran259@gmail.com>
2022-01-23 12:09:52 +0530

    add PartTimeEmployee

--- EmpWageComputation/EmpWageComputation/EmployeeAttendence.cs
+++ EmpWageComputation/EmpWageComputation/EmployeeAttendence.cs
@@ -8,22 +8,27 @@ namespace EmpWageComputation
 {
     internal class EmployeeAttendence
     {
-        public const int IS_FULL_TIME = 1, EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20;
         int empHrs = 0, empWage = 0;
         public void DailyEmployeeWage()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
             else
             {
-                empHrs = 0; ;
+
+                empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage: " + empWage);
+            Console.WriteLine("Employee Wage" + empWage);
         }
     }
 }
